ef07b8e411947e6b4951f15fde259c7a7453ca69
rintosaputro <saputrorinto@gmail.com>
2022-06-09 05:17:31 +0700

    style: linter adjusment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './assets/styles/custom.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
